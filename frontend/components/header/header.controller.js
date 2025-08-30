86310734193119fb7c999f9a5fbf0336e9637fa2
Ali Saeed <alisaeed541273@gmail.com>
2025-08-30 16:01:22 +0500

    new changes

--- frontend/components/header/header.controller.js
+++ frontend/components/header/header.controller.js
@@ -1,8 +1,4 @@
 app.controller("HeaderController", function ($scope) {
-  $scope.brand = "MyWebsite";
-  $scope.menuItems = [
-    { name: "Home", link: "#/" },
-    { name: "Services", link: "#/services" },
-    { name: "Contact", link: "#/contact" },
-  ];
+  $scope.isAuthenticated = true;
+  
 });
